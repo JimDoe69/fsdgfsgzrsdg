37c95a05c0b7b8a4906baafe4fdaa5bdcf408b4d
JimDoe69 <33529187+JimDoe69@users.noreply.github.com>
2017-12-05 10:54:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 
 bot.on('guildMemberAdd', function(member) {
-    member.guild.channels.find('name', 'general').sendMessage(member.toString() + ' Welcome To The NIGGER™ Discord, You Are Now A Nigger, For A Custom Nigger Role Type !customrole');
+    member.guild.channels.find('name', '#send').sendMessage(member.toString() + ' Welcome To The NIGGER™ Discord, You Are Now A Nigger, For A Custom Nigger Role Type !customrole');
 
     member.addRole(member.guild.roles.find('name', 'Member'));
 });
